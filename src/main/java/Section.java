1884ef9dc04c7cce11264b338e2a651a9c776d07
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2024-06-04 16:39:06 +0300

    Created Section with SectionName Attribute

---
+++ src/main/java/Section.java
@@ -0,0 +1,11 @@
+public abstract class Section implements Instrument {
+    protected String sectionName;
+
+    public Section(String sectionName){
+        this.sectionName = sectionName;
+    }
+    @Override
+    public void play() {
+        System.out.print("Playing music with " + sectionName);
+    }
+}
