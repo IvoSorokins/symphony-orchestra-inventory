110a59c1e7efa40986a0c77e12db1af88c7e3603
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2024-06-06 14:28:00 +0300

    Added addInstrument()

--- src/main/java/instruments/sections/StringsSection.java
+++ src/main/java/instruments/sections/StringsSection.java
@@ -1,8 +1,19 @@
 package instruments.sections;
 
+import instruments.Instrument;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringsSection extends Section {
+    private List<Instrument> instruments;
     public StringsSection(){
         super("strings section");
+        instruments = new ArrayList<>();
+    }
+
+    public <T extends Instrument> void addInstrument(T instrument) {
+        instruments.add(instrument);
     }
 
     @Override
--- src/main/java/instruments/sections/WoodwindsSection.java
+++ src/main/java/instruments/sections/WoodwindsSection.java
@@ -1,9 +1,21 @@
 package instruments.sections;
 
+import instruments.Instrument;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WoodwindsSection extends Section {
+    private List<Instrument> instruments;
 
     public WoodwindsSection(){
         super("Woodwinds section");
+        instruments = new ArrayList<>();
+    }
+
+    public <T extends Instrument> void addInstrument(T instrument) {
+        instruments.add(instrument);
+
     }
 
     @Override
