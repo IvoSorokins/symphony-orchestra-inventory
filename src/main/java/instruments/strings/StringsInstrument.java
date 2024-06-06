e7abe704af96506b9964fff60520a9caa33e6ee1
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2024-06-06 15:45:07 +0300

    Added manufacturer, id, availability and ID Prefix

--- src/main/java/instruments/strings/StringsInstrument.java
+++ src/main/java/instruments/strings/StringsInstrument.java
@@ -3,4 +3,9 @@ package instruments.strings;
 import instruments.Instrument;
 
 public abstract class StringsInstrument implements Instrument {
+    protected String manufacturer;
+    protected String id;
+    protected boolean isAvailable;
+
+    public static final String ID_PREFIX = "STRINGS-";
 }
--- src/main/java/instruments/woodwinds/WoodwindsInstrument.java
+++ src/main/java/instruments/woodwinds/WoodwindsInstrument.java
@@ -3,4 +3,9 @@ package instruments.woodwinds;
 import instruments.Instrument;
 
 public abstract class WoodwindsInstrument implements Instrument {
+    protected String manufacturer;
+    protected String id;
+    protected boolean isAvailable;
+
+    public static final String ID_PREFIX = "WOODWINDS-";
 }
