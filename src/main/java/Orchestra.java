232107f374eae56bd98057c15bab6cb49415e8ac
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2024-06-06 13:55:43 +0300

    Updated Main Class

--- src/main/java/Orchestra.java
+++ src/main/java/Orchestra.java
@@ -1,15 +1,21 @@
-import instruments.Instrument;
-import instruments.sections.StringsSection;
-import instruments.sections.WoodwindsSection;
+import instruments.strings.*;
+import instruments.woodwinds.*;
+import instruments.sections.*;
 
 
 public class Orchestra {
     public static void main(String[] args) {
-        Instrument strings = new StringsSection();
-        Instrument woodwinds = new WoodwindsSection();
+        StringsSection strings = new StringsSection();
+        WoodwindsSection woodwinds = new WoodwindsSection();
 
-        strings.play();
-        System.out.println();
-        woodwinds.play();
+
+        strings.addInstrument(new Violin("Stradivari"));
+        strings.addInstrument(new Cello("Yamaha"));
+        strings.addInstrument(new Violin("Knilling"));
+        strings.addInstrument(new Violin("Stentor"));
+
+
+        woodwinds.addInstrument(new Flute("Emerson"));
+        woodwinds.addInstrument(new Flute("Pearl"));
     }
 }
