62312f631e2dc108b522fd22bce8f51c7783f1c6
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2024-06-06 16:25:15 +0300

    Added Musician attributes + constructor for defining new musicians

--- src/main/java/musicians/Musician.java
+++ src/main/java/musicians/Musician.java
@@ -1,4 +1,11 @@
 package musicians;
 
 public abstract class Musician {
+    protected String name;
+    protected String instrumentID;
+
+    public Musician(String name){
+        this.name = name;
+    }
+
 }
