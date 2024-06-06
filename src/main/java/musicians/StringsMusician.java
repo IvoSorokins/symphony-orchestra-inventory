b6f2bfba632255f8ed9db175e2aa2c0747f0ee41
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2024-06-06 16:28:56 +0300

    Added takeInstrument method

--- src/main/java/musicians/StringsMusician.java
+++ src/main/java/musicians/StringsMusician.java
@@ -1,4 +1,10 @@
 package musicians;
 
+import instruments.Instrument;
+
 public class StringsMusician extends Musician{
+
+    public void takeInstrument(Instrument instrument){
+
+    }
 }
--- src/main/java/musicians/WoodwindsMusician.java
+++ src/main/java/musicians/WoodwindsMusician.java
@@ -1,4 +1,8 @@
 package musicians;
 
+import instruments.Instrument;
+
 public class WoodwindsMusician extends Musician{
+
+    public void takeInstrument(Instrument instrument){
 }
