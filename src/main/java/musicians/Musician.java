f847296e665f7c18a0facb155cd3fdd9750c7cba
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2024-06-07 16:59:20 +0300

    Added takeInstrument, getter and setter

--- src/main/java/musicians/Musician.java
+++ src/main/java/musicians/Musician.java
@@ -1,11 +1,27 @@
 package musicians;
 
+
+import instruments.Instrument;
+
+
 public abstract class Musician {
     protected String name;
     protected String instrumentID;
 
     public Musician(String name){
         this.name = name;
+        this.instrumentID = null;
     }
 
+    public String getInstrumentID(){
+        return instrumentID;
+    }
+
+    public void setInstrumentID(String instrumentID){
+        this.instrumentID = instrumentID;
+    }
+    public abstract void takeInstrument(Instrument instrument);
+
 }
+
+
