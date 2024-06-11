2c579ccc9d69091c7b872daaed1754556ce4d14d
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2024-06-11 23:13:12 +0300

    Implement takeInstrument() from superclass

--- src/main/java/musicians/StringsMusician.java
+++ src/main/java/musicians/StringsMusician.java
@@ -1,13 +1,28 @@
 package musicians;
 
 import instruments.Instrument;
+import instruments.strings.StringsInstrument;
 
-public class StringsMusician extends Musician{
-    public StringsMusician(String name){
+
+public class StringsMusician extends Musician {
+    public StringsMusician(String name) {
         super(name);
     }
 
-    public void takeInstrument(Instrument instrument){
 
+    @Override
+    public void takeInstrument(Instrument instrument) {
+        if (instrument instanceof StringsInstrument) {
+            StringsInstrument stringInstrument = (StringsInstrument) instrument;
+            if (stringInstrument.isAvailable()) {
+                this.instrumentID = stringInstrument.getID();
+                stringInstrument.setAvailable(false);
+            } else {
+                throw new IllegalArgumentException("Instrument is already taken.");
+            }
+        } else {
+            throw new IllegalArgumentException("Only string instruments are allowed.");
+        }
     }
 }
+
--- src/main/java/musicians/WoodwindsMusician.java
+++ src/main/java/musicians/WoodwindsMusician.java
@@ -1,6 +1,8 @@
 package musicians;
 
+
 import instruments.Instrument;
+import instruments.woodwinds.WoodwindsInstrument;
 
 public class WoodwindsMusician extends Musician {
 
@@ -8,6 +10,16 @@ public class WoodwindsMusician extends Musician {
         super(name);
     }
 
+    @Override
     public void takeInstrument(Instrument instrument) {
+        if (instrument instanceof WoodwindsInstrument) {
+            WoodwindsInstrument woodwindsInstrument = (WoodwindsInstrument) instrument;
+            if (woodwindsInstrument.isAvailable()) {
+                this.instrumentID = woodwindsInstrument.getID();
+                woodwindsInstrument.setAvailable(false);
+            } else {
+                throw new IllegalArgumentException("Instrument is already taken.");
+            }
+        }
     }
 }
